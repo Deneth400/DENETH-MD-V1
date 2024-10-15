bc1d70c3fd135a4c3b692e983a41f9f07cd53fe0
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-16 01:01:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID || "gmlEQQgK#I3p8_RkDchmZYAjNsuKWATOwQIJjyJW6C_ReK7-4oMY",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/denethhansaka/DENETH-MD-Files/blob/main/Images/DENETH-MD.jpg?raw=true", 
+ALIVE_MSG: process.env.ALIVE_MSG || "Hey,I am DENETH-MD Multi Device WA-BOT.I'm Alive Now😎.",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 };
