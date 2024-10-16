8512cae44c26b820b83128940057a93b7e8735d7
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-16 18:59:56 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,6 +10,14 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+let des = `*╔╭────────────╮╕*
+*╭│ⱧＥLLO, I'm Alive Now👾  │─◎◎▷*
+*╘╰────────────╯╜*
+*│A Qҽҽɳ-乙azie-MD Whatsapp Bot Based │Third Party Application Provide* *Many │Services With A RealTime* *Automated │Consversational* *Experience, Enjoy💫.*
+*│*
+*│Type .menu To Get Bot User*
+*│Menu💫*
+*╰───────────────◎◎▷*`
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
 }catch(e){
 console.log(e)
