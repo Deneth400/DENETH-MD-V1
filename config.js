401e1dc6a02c7d365f480527ebac48cd3dd92ea1
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-16 18:25:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "gmlEQQgK#I3p8_RkDchmZYAjNsuKWATOwQIJjyJW6C_ReK7-4oMY",
-MONGODB: process.env.MONGODB || "mongodb://mongo:GFkwUlmTAOpkmpzJCoPjmecqsZePZlwV@autorack.proxy.rlwy.net:57048",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/denethhansaka/DENETH-MD-Files/blob/main/Images/DENETH-MD.jpg?raw=true",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am Alive Now",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 
 };
