bdec1f79181bef8bd9fe032366872a512eb50dff
deneth_hansaka <167519066+denethhansaka@users.noreply.github.com>
2024-10-16 15:26:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '.'
 
 const ownerNumber = ['94761864425']
 
@@ -43,6 +42,10 @@ async function connectToWA() {
 const connectDB = require('./lib/mongodb')
 connectDB();
 //==========================================================================
+const {readEnv} = require('.lib/database')
+const config = await readEnv();
+const prefix = config.PREFIX
+//============================================================================
 console.log("CONNECTING DENETH-MD WA-BOT 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
